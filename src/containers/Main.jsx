63b0154dfb66448021c90c39f2960b56d3994394
kaiya li <kaiyali@microsoft.com>
2022-11-03 20:16:11 +0800

    change srtle

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -238,7 +238,7 @@ const Main = (props) => {
           }});
           // wx.miniProgram.navigateBack();
           wx.miniProgram.switchTab({url: "/pages/index/index"})
-        }}>返回</div>
+        }}> {"< 返回"}</div>
     </div>
   );
 }
